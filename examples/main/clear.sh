e19b0d322be68c41da4df7a10e9f20256e935ebc
Noskov Artem <nowasmawesome@gmail.com>
2019-06-10 11:02:41 +0500

    Removed unused files files

--- examples/main/clear.sh
+++ examples/main/clear.sh
@@ -2,4 +2,4 @@
 
 rm *_gas.go **/*_gas.go components/**/*_gas.go static/app.js static/app.js.map a.css a.min.css css/a.scss css/a.css .gaslock
 rm css/*.scss.css css/**/*.scss.css css/*.scss.css.map css/**/*.scss.css.map css/*.sass.css css/*.scss.css css/**/*.sass.css css/**/*.scss.css css/s.css css/se.css css/si.css css/s.scss css/se.scss css/si.scss css/tree.scss css/main.scss
-rm -r dist/ .sass-cache/ css/.sass-cache/ css/**/.sass-cache/
+rm -r dist/ .sass-cache/ css/.sass-cache/ css/**/.sass-cache/ web_modules
